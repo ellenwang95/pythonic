3a62b96f00d1f49dc186b42ebea2f58d8ec243d5
Ellen Wang <ellenwang95@yahoo.ca>
2015-12-12 18:03:25 -0500

    took out codeflask, took out examples.css

--- pythonic/__init__.py
+++ pythonic/__init__.py
@@ -3,3 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 from pythonic import views
 
+
+
--- pythonic/views.py
+++ pythonic/views.py
@@ -1,7 +1,23 @@
 from pythonic import app
 from flask import render_template, url_for
+import os
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+# to avoid caching css files
+# taken from: http://flask.pocoo.org/snippets/40/
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
